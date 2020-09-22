49f5e369c8d6b0a7ee6dd2c82685c78217d2d382
Lhoussaine <bouaik.lhou@gmail.com>
2020-09-23 00:14:23 +0100

    Setup phaser with webpack

---
+++ webpack.config.js
@@ -0,0 +1,46 @@
+const path = require('path');
+const webpack = require('webpack');
+
+module.exports = {
+  entry: {
+    app: './src/index.js',
+    'production-dependencies': ['phaser']
+  },
+
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'app.bundle.js'
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        include: path.resolve(__dirname, 'src'),
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['env']
+          }
+        }
+      }
+    ]
+  },
+
+  devServer: {
+    contentBase: path.resolve(__dirname, 'build'),
+    port: 8050
+  },
+
+
+  plugins: [
+    new webpack.DefinePlugin({
+      'typeof CANVAS_RENDERER': JSON.stringify(true),
+      'typeof WEBGL_RENDERER': JSON.stringify(true)
+    }),
+    new webpack.optimize.CommonsChunkPlugin({
+        name: 'production-dependencies',
+        filename: 'production-dependencies.bundle.js'
+    }),
+  ]
+};

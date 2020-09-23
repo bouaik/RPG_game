ad0ac99d530285a99465bc5a50e606f4aad8dd60
Lhoussaine <bouaik.lhou@gmail.com>
2020-09-23 03:29:34 +0100

    Make options to scene to enable music and audio

--- build/app.bundle.js
+++ build/app.bundle.js
@@ -305,7 +305,8 @@ var PreloaderScene = function (_Phaser$Scene) {
   }, {
     key: 'ready',
     value: function ready() {
-      this.scene.start('Credits');
+      1;
+      this.scene.start('Options');
       this.readyCount++;
       if (this.readyCount === 2) {
         this.scene.start('Title');
@@ -464,7 +465,54 @@ var OptionsScene = function (_Phaser$Scene) {
     value: function preload() {}
   }, {
     key: 'create',
-    value: function create() {}
+    value: function create() {
+      this.musicOn = true;
+      this.soundOn = true;
+
+      this.text = this.add.text(300, 100, 'Options', { fontSize: 40 });
+      this.musicButton = this.add.image(200, 200, 'checkedBox');
+      this.musicText = this.add.text(250, 190, 'Music Enabled', { fontSize: 24 });
+
+      this.soundButton = this.add.image(200, 300, 'checkedBox');
+      this.soundText = this.add.text(250, 290, 'Sound Enabled', { fontSize: 24 });
+
+      this.musicButton.setInteractive();
+      this.soundButton.setInteractive();
+
+      this.musicButton.on('pointerdown', function () {
+        this.musicOn = !this.musicOn;
+        this.updateAudio();
+      }.bind(this));
+
+      this.soundButton.on('pointerdown', function () {
+        this.soundOn = !this.soundOn;
+        this.updateAudio();
+      }.bind(this));
+
+      this.updateAudio();
+      this.menuButton = this.add.sprite(400, 500, 'blueButton1').setInteractive();
+      this.menuText = this.add.text(0, 0, 'Menu', { fontSize: '32px', fill: '#fff' });
+      Phaser.Display.Align.In.Center(this.menuText, this.menuButton);
+
+      this.menuButton.on('pointerdown', function (pointer) {
+        this.scene.start('Title');
+      }.bind(this));
+    }
+  }, {
+    key: 'updateAudio',
+    value: function updateAudio() {
+      if (this.musicOn === false) {
+        this.musicButton.setTexture('box');
+      } else {
+        this.musicButton.setTexture('checkedBox');
+      }
+
+      if (this.soundOn === false) {
+        this.soundButton.setTexture('box');
+      } else {
+        this.soundButton.setTexture('checkedBox');
+      }
+    }
   }]);
 
   return OptionsScene;
